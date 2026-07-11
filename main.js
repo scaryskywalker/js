2e2ace5cc651763e683c56d1327c966b304bd7e4
manu <298598440+scaryskywalker@users.noreply.github.com>
2026-07-11 18:57:59 +0530

    initialize

---
+++ main.js
@@ -0,0 +1,15 @@
+function add7(num) {
+  return num + 7;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function capitalize(a) {
+  return a.charAt(0).toUpperCase() + a.slice(1);
+}
+
+function last(a) {
+  return a.slice(-1);
+}
