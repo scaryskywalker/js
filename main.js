20ffb56acef4ebaac495ea85859d0c92e0b4e48d
manu <298598440+scaryskywalker@users.noreply.github.com>
2026-07-11 19:47:45 +0530

    guhi

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
-function add7(num) {
-  return num + 7;
+function getComputerChoice() {
+  value = Math.random();
+
+  if (value >= 0 && value <= 0.33) {
+    return "rock";
+  } else if (value >= 0.33 && value <= 0.66) {
+    return "paper";
+  } else if (value >= 0.66 && value <= 1) {
+    return "scissor";
+  }
 }
 
-function multiply(a, b) {
-  return a * b;
+function gethumanChoice(choice) {
+  choice = prompt("Enter your choice");
+  return choice;
 }
 
-function capitalize(a) {
-  return a.charAt(0).toUpperCase() + a.slice(1);
-}
+const cpu = getComputerChoice();
 
-function last(a) {
-  return a.slice(-1);
+const user = gethumanChoice();
+let total_human_points, total_cpu_points;
+
+if (user == "rock" && cpu == "paper") {
+  console.log("Cpu chose Paper!! It wins");
+} else if (user == "paper" && cpu == "rock") {
+  console.log("You wins!! Cpu chose rock");
+} else if (user == "scissor" && cpu == "paper") {
+  console.log("You wins!! Cpu chose paper");
+} else if (user == "paper" && cpu == "scissor") {
+  console.log("Cpu chose Scissor!! It wins");
+} else if (user == "scissor" && cpu == "rock") {
+  console.log("Cpu chose rock!! It wins.");
+} else if (user == "rock" && cpu == "scissor") {
+  console.log("You wins!! Cpu chose scissor");
+} else if (user == "rock" && cpu == "rock") {
+  console.log("Draw!!");
+} else if (user == "scissor" && cpu == "scissor") {
+  console.log("Draw!!");
+} else if (user == "paper" && cpu == "paper") {
+  console.log("Draw");
 }
